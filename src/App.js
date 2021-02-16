8cfa2d019650479e0fcd475e38eeb786bfcbdeb2
Kevin Klukowski <kklukowski12@gmail.com>
2021-02-15 21:02:37 -0500

    useReducer set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import initialState, { reducer } from './reducers';
 
 import './App.css';
 
@@ -6,6 +7,10 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+
 
   return (
     <div className="App">
